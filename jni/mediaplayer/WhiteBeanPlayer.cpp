1237acc86b067715b7e65539f52ab41fe78eb541
Lou Shuai <lou.shuai@hotmail.com>
2016-06-27 20:56:51 +0800

    Add player logic

--- jni/mediaplayer/WhiteBeanPlayer.cpp
+++ jni/mediaplayer/WhiteBeanPlayer.cpp
@@ -5,6 +5,7 @@
  *      Author: loushuai
  */
 
+#include "log.hpp"
 #include "WhiteBeanPlayer.hpp"
 
 extern "C" {
@@ -13,10 +14,31 @@ extern "C" {
 #include "libavfilter/avfiltergraph.h"	
 }
 
+using namespace std;
+
 namespace whitebean {
 
+struct WhiteBeanEvent : public TimedEventQueue::Event {
+	WhiteBeanEvent(WhiteBeanPlayer *player,
+				   void (WhiteBeanPlayer::*method)())
+	: mPlayer(player), mMethod(method) {}
+	
+	virtual ~WhiteBeanEvent() {}
+protected:
+	virtual void fire(TimedEventQueue *queue, int64_t /* now_us */) {
+		(mPlayer->*mMethod)();
+	}
+private:
+	WhiteBeanPlayer *mPlayer;
+	void (WhiteBeanPlayer::*mMethod)();
+};
+
 WhiteBeanPlayer::WhiteBeanPlayer()
+: mQueueStarted(false)
+, mFlags(0)
+, mIsAsyncPrepare(false)
 {
+	LOGD("WhiteBeanPlayer()");
 	av_register_all();
 	avcodec_register_all();
 	avfilter_register_all();	
@@ -24,7 +46,169 @@ WhiteBeanPlayer::WhiteBeanPlayer()
 
 WhiteBeanPlayer::~WhiteBeanPlayer()
 {
+	LOGD("~WhiteBeanPlayer()");
+}
 
+int WhiteBeanPlayer::setDataSource(const string uri)
+{
+	reset_l();
+
+	mUri = uri;
+	
+	{
+		unique_lock<mutex> autoLock(mStateLock);
+		mStats.mURI = mUri;
+	}
+
+	return 0;
+}
+
+int WhiteBeanPlayer::prepareAsync()
+{
+	unique_lock<mutex> autoLock(mLock);
+
+    if (mFlags & PREPARING) {
+		return -1;
+	}
+
+    mIsAsyncPrepare = true;
+    return prepareAsync_l();	
+}
+
+void WhiteBeanPlayer::reset()
+{
+	unique_lock<mutex> autoLock(mLock);
+	reset_l();
+}
+
+int WhiteBeanPlayer::prepare()
+{
+	unique_lock<mutex> autoLock(mLock);	
+
+    if (mFlags & PREPARED) {
+        return 0;
+    }
+
+    if (mFlags & PREPARING) {
+        return -1;
+    }
+
+	mIsAsyncPrepare = false;
+	int err = prepareAsync_l();
+	if (err != 0) {
+		return err;
+	}
+
+	// wait prepare finish
+	while (mFlags & PREPARING) {
+		mPreparedCondition.wait(autoLock);
+	}
+
+	LOGD("Prepare finish");
+	
+	return 0;
+}
+
+int WhiteBeanPlayer::prepareAsync_l()
+{
+    if (mFlags & PREPARING) {
+		return -1;
+	}
+
+    if (!mQueueStarted) {
+        mQueue.start();
+        mQueueStarted = true;
+    }
+
+    modifyFlags(PREPARING, SET);
+    mAsyncPrepareEvent = shared_ptr<TimedEventQueue::Event> (new WhiteBeanEvent(
+									   this, &WhiteBeanPlayer::onPrepareAsyncEvent));
+
+    mQueue.postEvent(mAsyncPrepareEvent);	
+	
+	return 0;
+}
+
+void WhiteBeanPlayer::onPrepareAsyncEvent()
+{
+	LOGD("onPrepareAsyncEvent");
+	int ret = 0;
+	unique_lock<mutex> autoLock(mLock);
+
+	mSourcePtr = shared_ptr<MediaSource>(new MediaSource);
+	ret = mSourcePtr->open(mUri);
+	if (ret != 0) {
+		// cancel prepare
+		LOGD("Set datasource error");
+		goto out;
+	}
+	
+	mSourcePtr->start();
+
+	modifyFlags((PREPARING|PREPARE_CANCELLED|PREPARING_CONNECTED), CLEAR);
+	modifyFlags(PREPARED, SET);
+ out:
+	mPreparedCondition.notify_all();
+}
+
+void WhiteBeanPlayer::reset_l()
+{
+	mUri = "";
+}
+
+int WhiteBeanPlayer::play()
+{
+	LOGD("Play enter");
+	
+	if (!(mFlags & PREPARED)) {
+		return -1;
+	}
+
+	modifyFlags(PLAYING, SET);
+
+	if (!mSourcePtr) {
+		return -1;
+	}
+
+	if (mSourcePtr->hasAudio()) {
+		LOGD("Has audio");
+		if (!mAudioPlayerPtr) {
+			mAudioPlayerPtr = shared_ptr<AudioPlayer>(new AudioPlayer);
+			if (!mAudioPlayerPtr) {
+				return -1;
+			}
+
+			mAudioPlayerPtr->setSource(mSourcePtr);
+			mAudioPlayerPtr->start();
+		}
+	}
+
+	return 0;
+}
+
+void WhiteBeanPlayer::modifyFlags(unsigned value, FlagMode mode)
+{
+    switch (mode) {
+        case SET:
+            mFlags |= value;
+            break;
+        case CLEAR:
+			//            if ((value & CACHE_UNDERRUN) && (mFlags & CACHE_UNDERRUN)) {
+			//                notifyListener_l(MEDIA_INFO, MEDIA_INFO_BUFFERING_END);
+			//            }
+            mFlags &= ~value;
+            break;
+        case ASSIGN:
+            mFlags = value;
+            break;
+        default:
+			break;
+    }
+
+	{
+		unique_lock<mutex> autoLock(mStateLock);
+		mStats.mFlags = mFlags;
+	}
 }
 	
 }
--- jni/mediaplayer/WhiteBeanPlayer.hpp
+++ jni/mediaplayer/WhiteBeanPlayer.hpp
@@ -8,12 +8,89 @@
 #ifndef JNI_MEDIAPLAYER_WHITEBEANPLAYER_H_
 #define JNI_MEDIAPLAYER_WHITEBEANPLAYER_H_
 
+#include <string>
+#include <mutex>
+#include <memory>
+#include "TimedEventQueue.h"
+#include "AudioPlayer.hpp"
 
 namespace whitebean {
 class WhiteBeanPlayer {
 public:
 	WhiteBeanPlayer();
 	~WhiteBeanPlayer();
+
+	int setDataSource(const std::string uri);
+	int prepare();
+	int prepareAsync();
+	void reset();
+	int play();
+
+private:
+    enum {
+        PLAYING             = 0x01,
+        LOOPING             = 0x02,
+        FIRST_FRAME         = 0x04,
+        PREPARING           = 0x08,
+        PREPARED            = 0x10,
+        AT_EOS              = 0x20,
+        PREPARE_CANCELLED   = 0x40,
+        CACHE_UNDERRUN      = 0x80,
+        AUDIO_AT_EOS        = 0x0100,
+        VIDEO_AT_EOS        = 0x0200,
+        AUTO_LOOPING        = 0x0400,
+
+        // We are basically done preparing but are currently buffering
+        // sufficient data to begin playback and finish the preparation phase
+        // for good.
+        PREPARING_CONNECTED = 0x0800,
+
+        // We're triggering a single video event to display the first frame
+        // after the seekpoint.
+        SEEK_PREVIEW        = 0x1000,
+
+        AUDIO_RUNNING       = 0x2000,
+        AUDIOPLAYER_STARTED = 0x4000,
+
+        INCOGNITO           = 0x8000,
+
+        TEXT_RUNNING        = 0x10000,
+        TEXTPLAYER_INITIALIZED  = 0x20000,
+
+        SLOW_DECODER_HACK   = 0x40000,
+    };
+	
+	mutable std::mutex mLock;
+	mutable std::mutex mStateLock;
+	std::condition_variable mPreparedCondition;
+	
+    TimedEventQueue mQueue;
+    bool mQueueStarted;
+	std::shared_ptr<MediaSource> mSourcePtr;
+	std::shared_ptr<AudioPlayer> mAudioPlayerPtr;	
+	
+    enum FlagMode {
+        SET,
+        CLEAR,
+        ASSIGN
+    };
+	void modifyFlags(unsigned value, FlagMode mode);
+
+	std::string mUri;
+	uint32_t mFlags;
+	bool mIsAsyncPrepare;
+
+	std::shared_ptr<TimedEventQueue::Event> mAsyncPrepareEvent;
+	
+	struct Stats {
+		std::string mURI;
+		uint32_t mFlags;	
+	} mStats;
+
+	int prepareAsync_l();
+	void onPrepareAsyncEvent();
+	void reset_l();
+
 };
 
 	
